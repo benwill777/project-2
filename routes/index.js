834221f19706cffc94093af52cf9995be8cd6f22
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-15 19:27:20 -0400

    trying to get landing page to start at restaurants index

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', {
-    title: ' You Hit FohShow'
+  res.render('/restaurants', {
+    title: ' You Hit FohShow',
   });
 });
 
